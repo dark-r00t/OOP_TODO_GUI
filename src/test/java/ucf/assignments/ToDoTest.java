2e549847f8e4a4452732d7c2a52ef6b16ebb1c57
darkr00t <penialtoorpolute881@gmail.com>
2021-07-03 11:07:57 -0400

    Initial JUNIT Test Set-Up

---
+++ src/test/java/ucf/assignments/ToDoTest.java
@@ -0,0 +1,87 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Jan Darge
+ */
+
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoTest {
+
+    @Test
+    void addItem_ToDoList_Test(){
+        // create a LinkedList to test correct response
+        // create a LinkedList to throw in test items
+        // - name
+        // - a ToDoItem
+        // - amount of items in list
+        // call the addItem function w/ temp variables as strings
+        // assert that the linked lists are the same
+
+
+    }
+
+    void removeItem_ToDoList_Test(){
+        // create a LinkedList to test correct response
+        // create an empty LinkedList to check if items were removed
+        // call the addItem function w/ temp variables as strings
+        // then remove the item
+        // assert that the linked lists are the same
+
+    }
+
+    void editName_ToDoList_Test(){
+        // create a LinkedList to test correct response
+        // give the linked list random information
+        // create a second LinkedList w/ the desired response
+        // call editName w/ the newName and the firstList
+        // assert that the two linked lists are the same
+
+    }
+
+    void markAsComplete_ToDoController_Test(){
+
+
+
+    }
+
+    void deleteTask_ToDoController_Test(){
+
+
+
+    }
+
+    void addNewTask_ToDoController_Test(){
+
+
+
+    }
+
+    void editDescriptionInit_ToDoController_Test(){
+
+
+
+    }
+
+    void changeDateInit_ToDoController_Test(){
+
+
+
+    }
+
+    void modifyDateText_ToDoController_Test(){
+
+
+
+    }
+
+    void save_ToDoController_Test(){
+
+
+
+    }
+
+}
